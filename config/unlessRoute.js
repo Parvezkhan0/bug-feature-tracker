5c1ec4ea4fbf5ab631f53c21a0fc5a6626e8cbd8
Parvez Khan <Parvezkhan02@outlook.com>
2025-08-16 00:06:17 +0530

    fix route paths in unlessRoute config

--- config/unlessRoute.js
+++ config/unlessRoute.js
@@ -1,9 +1,9 @@
 module.exports = {
     authenticateRoutes: {
       path: [
-        { url: "/api/auth/register", methods: ["POST"] },
-        { url: "/api/auth/login", methods: ["POST"] },
-        { url: "/", methods: ["GET"] }  // Also exclude the health check
+        { url: "/register", methods: ["POST"] },
+        { url: "/login", methods: ["POST"] },
+        { url: "/", methods: ["GET"] }
       ]
     }
 };
